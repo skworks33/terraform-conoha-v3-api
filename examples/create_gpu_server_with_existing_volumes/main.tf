ad49ef62b00426c54d24d8373c10324d1059defa
Shohei Kodama <shohei-kodama@gmo.jp>
2024-07-18 14:26:06 +0900

    feat: Add Terraform scripts to create GPU server with existing volumes

---
+++ examples/create_gpu_server_with_existing_volumes/main.tf
@@ -0,0 +1,85 @@
+
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "~> 2.0.0"
+    }
+  }
+}
+
+provider "openstack" {
+  domain_name = "gnc" # ConoHa VPS は gnc 固定
+  auth_url    = var.conoha_v3_auth_url
+  tenant_name = var.conoha_v3_tenant_name
+  user_name   = var.conoha_v3_user_name
+  password    = var.conoha_v3_password
+}
+
+# SSHキーペア作成
+resource "openstack_compute_keypair_v2" "keypair" {
+  name = var.ssh_keypair_name
+  public_key = var.ssh_keypair_public_key
+}
+
+# フレーバー取得
+data "openstack_compute_flavor_v2" "flavor" {
+  name = var.flavor_name
+}
+
+# 既存のブートボリュームを取得
+# 注意: ConoHaでは同一名で複数のボリュームを作成できますが terraform-provider-openstack では名前で一意に特定するため、同一名のボリュームが複数ある場合はエラーになります
+data "openstack_blockstorage_volume_v3" "existing_boot_volume" {
+  name = var.boot_volume_name
+}
+
+# 既存の追加ボリュームを取得
+# 注意: ConoHaでは同一名で複数のボリュームを作成できますが terraform-provider-openstack では名前で一意に特定するため、同一名のボリュームが複数ある場合はエラーになります
+data "openstack_blockstorage_volume_v3" "existing_additional_volume" {
+  name = var.additional_volume_name
+}
+
+# ユーザーデータ取得
+data "template_file" "user_data" {
+  template = file("${path.module}/../../files/user_data.tpl")
+  vars = {
+    hostname = var.instance_name
+  }
+}
+
+# インスタンス作成
+# https://doc.conoha.jp/api-vps3/compute-create_vm-v3/
+# https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_instance_v2
+resource "openstack_compute_instance_v2" "instance" {
+  name = var.instance_name
+  flavor_id = data.openstack_compute_flavor_v2.flavor.id
+  key_pair = openstack_compute_keypair_v2.keypair.name
+  security_groups = [
+    var.default_security_group
+  ]
+  admin_pass = var.instance_root_password # 未指定時はランダムパスワードが設定される
+  metadata = {
+    instance_name_tag = var.instance_name # コントロールパネルで表示されるインスタンス名
+  }
+  user_data = data.template_file.user_data.rendered
+
+  # ブートディスクをアタッチ
+  block_device {
+    uuid = data.openstack_blockstorage_volume_v3.existing_boot_volume.id
+    source_type = "volume"
+    destination_type = "volume"
+    # boot_index は ConoHa 側で自動設定される
+  }
+
+  # 追加ディスクをアタッチ
+  block_device {
+    uuid = data.openstack_blockstorage_volume_v3.existing_additional_volume.id
+    source_type = "volume"
+    destination_type = "volume"
+    # boot_index は ConoHa 側で自動設定される
+  }
+
+  depends_on = [
+    openstack_compute_keypair_v2.keypair,
+  ]
+}
---
+++ examples/create_gpu_server_with_existing_volumes/variables.tf
@@ -0,0 +1,55 @@
+variable "conoha_v3_auth_url" {
+  description = "The authentication URL for ConoHa VPS V3."
+  default     = "https://identity.c3j1.conoha.io/v3"
+}
+
+variable "conoha_v3_tenant_name" {
+  description = "The tenant name (project name) for ConoHa VPS V3."
+}
+
+variable "conoha_v3_user_name" {
+  description = "The user name for ConoHa VPS V3."
+}
+
+variable "conoha_v3_password" {
+  description = "The password for ConoHa VPS V3."
+}
+
+variable "ssh_keypair_name" {
+  description = "The name of the SSH key pair to be used for the instance."
+  default     = "your-terraform-keypair-name"
+}
+
+variable "ssh_keypair_public_key" {
+  description = "The public key of the SSH key pair to be used for the instance."
+}
+
+variable "default_security_group" {
+  description = "The name of the default security group to be used for the instance."
+  default     = "default"
+}
+
+variable "flavor_name" {
+  description = "The name of the flavor to be used for the instance."
+  default     = "g2l-t-c20m128g1-l4" # 20vCPU, 128GB RAM, 100GB Disk, NVIDIA L4 GPU
+}
+
+variable "boot_volume_name" {
+  description = "The name of the boot volume to be used for the instance."
+  default     = "your-terraform-boot-volume-name"
+}
+
+variable "additional_volume_name" {
+  description = "The name of the additional volume to be used for the instance."
+  default     = "your-terraform-additional-volume-name"
+}
+
+variable "instance_name" {
+  description = "The name of the instance to be created."
+  default = "your-terraform-instance-name"
+}
+
+variable "instance_root_password" {
+  description = "The root password of the instance to be created."
+}
+
